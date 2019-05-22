67789bf6e87c16711d5b13af8b7c770cda43e47d
Eric Petersen <eric@kimonocloud.com>
2019-05-22 16:06:39 -0600

    WIP

--- src/main/java/kimono/client/impl/DefaultTenantInfoSupplier.java
+++ src/main/java/kimono/client/impl/DefaultTenantInfoSupplier.java
@@ -1,7 +1,7 @@
 package kimono.client.impl;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -15,10 +15,9 @@ import kimono.client.KCTenantInfoSupplier;
 import kimono.client.KimonoApiException;
 
 /**
- * Default {@link KCTenantInfoSupplier} implementation gets {@link TenantInfo}
- * instances from the Tenants API, optionally limited to specific Integrations. 
- * Alternatively, you can specify a specific actor ID to return 
- * {@link TenantInfo} for that actor only.
+ * Default {@link KCTenantInfoSupplier} implementation supplies
+ * {@link TenantInfo} instances by calling the Kimono Tenants API, optionally
+ * limited to specific Integrations by name or specific actors by Actor ID.
  */
 public class DefaultTenantInfoSupplier implements KCTenantInfoSupplier {
 
@@ -26,69 +25,74 @@ public class DefaultTenantInfoSupplier implements KCTenantInfoSupplier {
 	 * The Tenants API
 	 */
 	private TenantsApi api;
-	
+
 	/**
-	 * The UUID of an actor when configured to return TenantInfo for a specific 
-	 * actor. Otherwise, returns TenantInfo for all actors of all Integrations
-	 * identified by {@link #integrations}
+	 * The UUIDs of the actors to limit results to.
+	 * 
+	 * @see #withName(String...)
 	 */
-	private UUID actorId;
-	
+	private Set<UUID> filterByActorId = new HashSet<>();
+
 	/**
-	 * A list of Integration names to limit tenants to.
-	 * @see #setIntegrations(Collection)
+	 * The names of the Integrations to limit results to.
+	 * 
+	 * @see #withActorId(UUID...)
 	 */
-	private List<String> integrations;
-	
-	/**
-	 * Constructor
-	 * @param client The ApiClient to use to call the Tenants API. The
-	 * 	Tenants API uses API Key authentication rather than Actor Authentication,
-	 * 	so you must supply an ApiClient that is configured with an API Key
-	 * 	for Http Basic Authentication
-	 */
-	public DefaultTenantInfoSupplier( ApiClient client ) {
-		this(client,Collections.emptySet(),null);
-	}
-	
+	private Set<String> filterByName = new HashSet<>();
+
 	/**
 	 * Constructor
-	 * @param client The ApiClient to use to call the Tenants API. The
-	 * 	Tenants API uses API Key authentication rather than Actor Authentication,
-	 * 	so you must supply an ApiClient that is configured with an API Key
-	 * 	for Http Basic Authentication
-	 * @param integrations Optional set of Integration names to limit the response to.
-	 * 	See {@link #setIntegrations(Collection)}
-	 * @param actorId Optional actor ID to limit the response to
+	 * 
+	 * @param client The ApiClient to use to call the Tenants API. The Tenants API
+	 *               uses API Key authentication rather than Actor Authentication,
+	 *               so you must supply an ApiClient that is configured with an API
+	 *               Key for Http Basic Authentication
 	 */
-	public DefaultTenantInfoSupplier( ApiClient client, Set<String> integrations, UUID actorId ) {
-		this.api = new TenantsApi(client);
-		this.actorId = actorId;
-		setIntegrations(integrations);
+	public DefaultTenantInfoSupplier(ApiClient client) {
+		api = new TenantsApi(client);
 	}
-	
+
 	/**
-	 * A list of Integration names to limit tenants to. If you have a single
-	 * client that processes data on behalf of multiple Integrations, either
-	 * representing different products or variants of the same Integration,
-	 * use the form of constructor that accepts 
+	 * Return only tenants of a specific Integration or Integrations.
+	 * 
+	 * @param name The name of the Integration as specified in the Integration
+	 *             Blueprint
 	 */
-	public void setIntegrations( Collection<String> integrations ) {
-		// Convert to a List as required by Swagger
-		if( integrations != null ) {
-			this.integrations = integrations.stream().collect(Collectors.toList());
-		} else {
-			this.integrations = Collections.emptyList();
+	public KCTenantInfoSupplier withName(String... names) {
+		for (String name : names) {
+			filterByName.add(name);
 		}
+		return this;
 	}
-	
+
+	/**
+	 * Return only tenants having specific Actor Ids
+	 * 
+	 * @param name One or more Actor Ids
+	 */
+	public KCTenantInfoSupplier withActorId(UUID... ids) {
+		for (UUID id : ids) {
+			filterByActorId.add(id);
+		}
+		return this;
+	}
+
 	@Override
 	public List<TenantInfo> get() throws KimonoApiException {
 		try {
-			// TODO: Support specifying a single actorId
-			return api.listInteropTenants(integrations);
-		} catch( ApiException ex ) {
+			if (!filterByActorId.isEmpty()) {
+				return findByIds();
+			} else {
+				List<String> names = filterByName.stream().collect(Collectors.toList());
+				return api.listInteropTenants(names);
+			}
+		} catch (ApiException ex) {
 			throw new KimonoApiException(ex);
 		}
+	}
+
+	protected List<TenantInfo> findByIds() throws KimonoApiException {
+		// TODO: Not yet implemented
+		return Collections.emptyList();
 	}
 }
