e3be2cc00fe21c7b6d59a14cf69058abe1c17b8d
Eric Petersen <eric@kimonocloud.com>
2019-05-29 19:14:05 -0600

    Updated/WIP

--- src/main/java/kimono/client/KimonoApiException.java
+++ src/main/java/kimono/client/KimonoApiException.java
@@ -13,4 +13,54 @@ public class KimonoApiException extends Exception {
 	public KimonoApiException( Exception cause ) {
 		super(cause);
 	}
+	
+	public KimonoApiException( String msg, kimono.api.v2.interop.ApiException ex ) {
+		super(formatMessage(msg,ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( kimono.api.v2.interop.ApiException ex ) {
+		super(formatMessage(ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( kimono.api.v2.interopdata.ApiException ex ) {
+		super(formatMessage(ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( String msg, kimono.api.v2.interopdata.ApiException ex ) {
+		super(formatMessage(msg,ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( kimono.api.v2.broker.ApiException ex ) {
+		super(formatMessage(ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( String msg, kimono.api.v2.broker.ApiException ex ) {
+		super(formatMessage(msg,ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( kimono.api.v2.platform.ApiException ex ) {
+		super(formatMessage(ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( String msg, kimono.api.v2.platform.ApiException ex ) {
+		super(formatMessage(msg,ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( kimono.api.v2.sifcloud.ApiException ex ) {
+		super(formatMessage(ex.getMessage(),ex.getResponseBody()),ex);
+	}
+	public KimonoApiException( String msg,kimono.api.v2.sifcloud.ApiException ex ) {
+		super(formatMessage(msg,ex.getMessage(),ex.getResponseBody()),ex);
+	}
+
+	public static String formatMessage( String exception, String jsonResponseBody ) {
+		return formatMessage(null,exception,jsonResponseBody);
+	}
+	public static String formatMessage( String message, String exception, String jsonResponseBody ) {
+		StringBuilder b = new StringBuilder();
+		if( message != null ) {
+			b.append(message);
+		}
+		if( b.length() > 0 ) {
+			b.append(". ");
+		}
+		b.append(exception);
+		if( jsonResponseBody != null ) {
+			b.append(". ");
+		}
+		b.append(jsonResponseBody);
+		return b.toString();
+	}
 }
